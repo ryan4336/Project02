70e6ad3715a6ce19963aa08cf5aa9bd7abe0eb17
ryan4336 <rahhhp@umsystem.edu>
2025-02-28 17:52:42 -0600

    Created Elf class
    override methods for attack and getRace

---
+++ src/project02/MiddleEarthCharacters/Elf.java
@@ -0,0 +1,30 @@
+package project02.MiddleEarthCharacters;
+
+public class Elf extends MiddleEarthCharacter {
+	
+	public Elf(String name, double health, double power) {
+		super(name, health, power);
+	}
+
+	@Override
+	public boolean attack(MiddleEarthCharacter target) {
+		if(target.getRace() == "Orc") {
+			target.health -= power * 1.5;
+			System.out.println("Super effective attack! 1.5x damage!");
+			return true;
+		}
+		if(target.getRace().equals("Elf") || target.getRace().equals("Dwarf")) {
+			System.out.println("Attack inneffective! 0 damage done!");
+			return false;
+		}
+		target.health -= power;
+		System.out.println("Attack successful!");
+		return true;
+	}
+
+	@Override
+	public String getRace() {
+		return "Elf";
+	}
+	
+}
