6e0a362d51bba7a3bd748b57c00568c153c5bab6
ryan4336 <rahhhp@umsystem.edu>
2025-02-28 18:06:30 -0600

    Created Human class
    Override method attack() to function properly depending on type of
    target. Override getRace() to return Human.

---
+++ src/project02/MiddleEarthCharacters/Human.java
@@ -0,0 +1,31 @@
+package project02.MiddleEarthCharacters;
+
+public class Human extends MiddleEarthCharacter {
+
+	public Human(String name, double health, double power) {
+		super(name, health, power);
+	}
+
+	@Override
+	boolean attack(MiddleEarthCharacter target) {
+		if(target.getRace() == "Wizard") {
+			target.health -= power * 1.5;
+			System.out.println("Super effective attack! 1.5x damage!");
+			return true;
+		}
+		if(target.getRace().equals("Orc") || target.getRace().equals("Human")) {
+			System.out.println("Attack inneffective! 0 damage done!");
+			return false;
+		}
+		target.health -= power;
+		System.out.println("Attack successful!");
+		return true;
+	}
+
+	@Override
+	String getRace() {
+		return "Human";
+	}
+	
+	
+}
