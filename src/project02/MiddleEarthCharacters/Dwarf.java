db2edde65917b867ac0a6699cb35c3e4b5a34145
ryan4336 <rahhhp@umsystem.edu>
2025-02-28 18:09:48 -0600

    Updated Dwarf class
    Override method attack() to function properly depending on type of
    target. Override getRace() to return Dwarf.
    - (Meant to commit earlier but hadn't saved code written, only the
    creation of the class itself)

--- src/project02/MiddleEarthCharacters/Dwarf.java
+++ src/project02/MiddleEarthCharacters/Dwarf.java
@@ -1,5 +1,31 @@
 package project02.MiddleEarthCharacters;
 
-public class Dwarf {
+public class Dwarf extends MiddleEarthCharacter {
 
+	public Dwarf(String name, double health, double power) {
+		super(name, health, power);
+	}
+
+	@Override
+	boolean attack(MiddleEarthCharacter target) {
+		if(target.getRace() == "Elf") {
+			target.health -= power * 1.5;
+			System.out.println("Super effective attack! 1.5x damage!");
+			return true;
+		}
+		if(target.getRace().equals("Wizard") || target.getRace().equals("Dwarf")) {
+			System.out.println("Attack inneffective! 0 damage done!");
+			return false;
+		}
+		target.health -= power;
+		System.out.println("Attack successful!");
+		return true;
+	}
+
+	@Override
+	String getRace() {
+		return "Dwarf";
+	}
+	
+	
 }
