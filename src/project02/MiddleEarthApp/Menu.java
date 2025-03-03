d53aea732777fbe7ce6ec2dfc2c97d1e639f8318
ryan4336 <rahhhp@umsystem.edu>
2025-03-02 18:11:40 -0600

    Added Menu class and implemented menu option methods addCharacter and viewCharacters
    addCharacters prompts user for character info and error checks inputs.
    Then creates a character from user's inputs.
    viewCharacters simply calls displayAllCharacters method and displays all
    character info.

---
+++ src/project02/MiddleEarthApp/Menu.java
@@ -0,0 +1,151 @@
+package project02.MiddleEarthApp;
+
+import java.util.Scanner;
+
+import project02.CharacterManager.CharacterManager;
+import project02.CharacterManager.MiddleEarthCouncil;
+import project02.MiddleEarthCharacters.Dwarf;
+import project02.MiddleEarthCharacters.Elf;
+import project02.MiddleEarthCharacters.Human;
+import project02.MiddleEarthCharacters.Orc;
+import project02.MiddleEarthCharacters.Wizard;
+
+public class Menu {
+	
+	public static void displayMenu(Scanner scanner) {
+		System.out.println("\nWhat would you like to do?\n-----------------------\n");
+		System.out.println("1. Add a new character.");
+		System.out.println("2. View all characters.");
+		System.out.println("3. Update a character.");
+		System.out.println("4. Delete a character.");
+		System.out.println("5. Execute all characters' attack actions.");
+		System.out.println("6. Exit.");
+		
+		int userChoice;
+		
+		
+		while(true) {
+			System.out.println("\nPlease enter a number (1-6): ");
+
+			try {
+				userChoice = scanner.nextInt();
+				scanner.nextLine();
+				break;
+			}catch(Exception e){
+				System.out.println("Error: Must enter an integer.");
+				scanner.nextLine();
+				continue;
+			}
+		}
+		
+		
+		
+		switch(userChoice) {
+			case 1:
+				Menu.addCharacter(scanner);
+				break;
+			case 2:
+				Menu.viewCharacters(scanner);
+				break;
+			case 3:
+				
+				break;
+			case 4:
+				
+				break;
+			case 5:
+				
+				break;
+			case 6:
+				
+				System.out.println("\nExiting program...");
+				scanner.close();
+				System.exit(0);
+				
+		}
+		
+	}
+	
+	
+	private static void addCharacter(Scanner scanner) {
+		
+		String name;
+		int health;
+		int power;
+		
+		System.out.println("Enter the new character's name: ");
+		name = scanner.nextLine();
+		
+		System.out.println("Enter the new character's health: ");
+		while(!scanner.hasNextInt()) {
+			System.out.println("Error: Must enter an integer value.");
+			scanner.next();
+		}
+		health = scanner.nextInt();
+		scanner.nextLine();
+		
+		
+		System.out.println("Enter the new character's power: ");
+		while(!scanner.hasNextInt()) {
+			System.out.println("Error: Must enter an integer value.");
+			scanner.next();
+		}
+		power = scanner.nextInt();
+		scanner.nextLine();
+		
+		String race;
+		while(true) {
+			System.out.println("Enter the new character's race: ");
+			race = scanner.nextLine().toLowerCase();
+			
+			
+			if(!race.equals("elf") && !race.equals("dwarf") && !race.equals("wizard") && !race.equals("orc") && !race.equals("human")) {
+				System.out.println("ERROR: Race \"" + race + "\" not available. Please choose Elf, Orc, Wizard, Human, or Dwarf.");
+				continue;	
+			}
+			break;
+		}
+					
+			
+		
+		
+		MiddleEarthCouncil instance = MiddleEarthCouncil.getInstance();
+		CharacterManager characterManager = instance.getCharacterManager();
+		
+		switch(race) {
+		case "orc":
+			Orc newOrc = new Orc(name, health, power);
+			characterManager.addCharacter(newOrc);
+			break;
+		case "elf":
+			Elf newElf = new Elf(name, health, power);
+			characterManager.addCharacter(newElf);
+			break;
+		case "dwarf":
+			Dwarf newDwarf = new Dwarf(name, health, power);
+			characterManager.addCharacter(newDwarf);
+			break;
+		case "human":
+			Human newHuman = new Human(name, health, power);
+			characterManager.addCharacter(newHuman);
+			break;
+		case "wizard":
+			Wizard newWizard = new Wizard(name, health, power);
+			characterManager.addCharacter(newWizard);
+			break;
+			
+		}
+		Menu.displayMenu(scanner);
+	}
+	
+	
+	public static void viewCharacters(Scanner scanner) {
+		MiddleEarthCouncil instance = MiddleEarthCouncil.getInstance();
+		CharacterManager characterManager = instance.getCharacterManager();
+		
+		characterManager.displayAllCharacters();
+		
+		Menu.displayMenu(scanner);
+	}
+	
+}
